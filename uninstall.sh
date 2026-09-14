63fb5d4394da285df863c3e393df953c39f170c5
DjGreeZ <dj.greez@gmail.com>
2026-09-15 06:08:51 +0700

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-[ "$(id -u)" = 0 ] || { echo 'Запустите от root.'; exit 1; }
-case "${1-}" in ''|--purge) ;; *) echo 'Использование: uninstall.sh [--purge]'; exit 1;; esac
-python3 /data/awg-native/uninstall.py "$@"
